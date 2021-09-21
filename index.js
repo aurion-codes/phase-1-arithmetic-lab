737d4eed403b72682a53ce8e8fd04b90bcb6bcb0
AurionHoward <aurionhoward@gmail.com>
2021-09-21 00:54:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+const random = 1;
+console.log(Number.isInteger(random));
+
+const num3 = 29
+const num4 = 5
+let mod = num3 % num4;
+
+const max= Math.max(2, 6, 20, 9);
+console.log(max);
+
+
+
+
+
+
+
+
+
+    
+
+     
+    
+
