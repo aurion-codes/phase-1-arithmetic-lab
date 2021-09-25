076cb155e8d07a711c77218d24f283f70c5c0d0f
AurionHoward <aurionhoward@gmail.com>
2021-09-25 11:34:13 -0400

    commit tree 2f3c6d692e8ff6173079dc6b028349e3ad526b6b

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const num1 = 31;
-const num2 = 2;
-let multiply = num1 * num2;
 
-
-const random = 1;
-console.log(Number.isInteger(random));
-
-const num3 = 29
-const num4 = 5
-let mod = num3 % num4;
-
-const max= Math.max(2, 6, 20, 9);
-console.log(max);
+function multiplpy(num1, num2){
+return num1 * num2;
+}
 
 
 
